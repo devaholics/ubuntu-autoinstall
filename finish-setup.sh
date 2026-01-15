6e856631c40b15c0e49c9507e447c13cd1f4b8d4
Philipp Sommersguter <philipp@devaholics.io>
2026-01-15 19:30:03 +0100

    Install Chrome and enable the first-run.service via cloud-init

--- finish-setup.sh
+++ finish-setup.sh
@@ -15,14 +15,18 @@ gsettings set org.gnome.system.locale region 'de_AT.UTF-8'
 #  LC_MEASUREMENT=de_AT.UTF-8 \
 #  LC_IDENTIFICATION=de_AT.UTF-8
 
-echo "Setting desktop favorites..."
-gsettings set org.gnome.shell favorite-apps "['firefox_firefox.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Ptyxis.desktop', 'bitwarden_bitwarden.desktop', 'slack_slack.desktop']"
-
 echo "Installing Node Version Manager v0.40.3..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 
 echo "Installing SDKMan..."
 curl -s "https://get.sdkman.io" | bash
+
+echo "Installing Google Chrome"
+wget -O ~/Downloads/google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i ~/Downloads/google-chrome-stable_current_amd64.deb
+
+echo "Setting desktop favorites..."
+gsettings set org.gnome.shell favorite-apps "['google-chrome.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Ptyxis.desktop', 'bitwarden_bitwarden.desktop', 'slack_slack.desktop']"
 
 echo "Validate the installation and then press any key to close this window."
 read -s -r
